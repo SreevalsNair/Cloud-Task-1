bb8faa59bc996df99d0a893cb8afecd3df3d20cb
Sreelmaooo <nairsreevals@gmail.com>
2026-08-03 11:53:38 +0530

    Fix API_BASE URL by removing IP address

--- expense-approval-system-main/expense-approval-system-main/frontend/src/api.js
+++ expense-approval-system-main/expense-approval-system-main/frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://3.111.158.52:8080";
+const API_BASE = "http://:8080";
 
 async function request(path, options = {}) {
   const response = await fetch(`${API_BASE}${path}`, {
