42b8bdbfb6365a85809e0a9be1fee264e8310042
Sreelmaooo <nairsreevals@gmail.com>
2026-08-03 18:36:54 +0530

    Update AuthController.java

--- expense-approval-system-main/expense-approval-system-main/ExpenseApproval/src/main/java/com/example/ExpenseApproval/controller/AuthController.java
+++ expense-approval-system-main/expense-approval-system-main/ExpenseApproval/src/main/java/com/example/ExpenseApproval/controller/AuthController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = {"http://13.232.7.225"})
+@CrossOrigin(origins = {"http://13.232.7.225:5173"})
 @RequestMapping("/auth")
 public class AuthController {
 
